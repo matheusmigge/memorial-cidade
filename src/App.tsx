dddef37acba423a86353c93e2d323b6b532409c7
matheusmigge <matheusmigge@gmail.com>
2025-01-30 15:23:26 -0300

    map skin configuration

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <MapContainer center={[-8.0433112, -34.934217]} zoom={13}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        // url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" <<< minimalist version 
       />
     </MapContainer>
   );
